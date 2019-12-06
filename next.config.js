cca25d34800e3f5e33dffd6478933a586179f0c4
Martin Hochel <hochelmartin@gmail.com>
2019-12-07 00:49:23 +0100

    build: add css plugin to next

---
+++ next.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+const withCSS = require('@zeit/next-css')
+
+const cfg = {
+  /**
+   * @type {import('./next-config').WebpackConfig}
+   */
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.raw.css$/,
+      use: 'raw-loader',
+    })
+    return config
+  },
+}
+
+const config = withCSS({
+  /* config options here */
+  ...cfg,
+})
+
+module.exports = config
