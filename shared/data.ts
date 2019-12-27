1129e6937e776e51d91e1460e187de728de646b1
Martin Hochel <hochelmartin@gmail.com>
2019-12-27 14:48:52 +0100

    shared: create shared module with data

---
+++ shared/data.ts
@@ -0,0 +1,22 @@
+const links = {
+  contact: {
+    email: {
+      link: 'mailto:ng@ngparty.cz',
+      label: 'ng@ngparty.cz',
+    },
+  },
+  more: [
+    {
+      label: 'Code of Conduct',
+      link: 'https://confcodeofconduct.com/',
+    },
+    {
+      label: 'Brand Materials',
+      link: 'http://bit.ly/ngParty-brand-materials',
+    },
+  ],
+}
+
+export const data = {
+  links,
+}
